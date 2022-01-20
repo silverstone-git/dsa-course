9388a329c83575a878c2d03e7c23f13d4e6f340c
silverstone-git <silverstone965@gmail.com>
2022-01-20 23:43:49 +0530

    Fixed the binary adder using a strings for bits and, std string for substring or concatenation

--- transform_number/binary_sum_6p3.cpp
+++ transform_number/binary_sum_6p3.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 #include <string>
 
-std::string adder(std::string n1, std::string n2, std::string *carry) {
+std::string adder(char n1, char n2, char *carry) {
 
     std::string resultbit;
-    if( n1 == "1" && n2 == "1") {
+    if( n1 == '1' && n2 == '1') {
         resultbit = "0";
-        *carry = "1";
-    } else if((n1 == "0" && n2 == "1") ||(n1 == "1" && n2 == "0")) {
+        *carry = '1';
+    } else if((n1 == '0' && n2 == '1') ||(n1 == '1' && n2 == '0')) {
         resultbit = "1";
-        *carry = "0";
+        *carry = '0';
     } else {
         resultbit = "0";
-        *carry = "0";
+        *carry = '0';
     }
 
     return resultbit;
@@ -22,19 +22,21 @@ std::string adder(std::string n1, std::string n2, std::string *carry) {
 }
 
 
-std::string tripleadder(std::string n1, std::string n2, std::string *carry) {
+std::string tripleadder(char n1, char n2, char *carry) {
 
     // this is a triple adder because a carry bit of 1 is assumed here
 
-    if(n1 == "0" && n2 == "0") {
-        *carry = "0";
-        return "1";
-    } else if(n1 == "0" && n2 == "1" || n1 == "1" && n2 == "0") {
-        *carry = "1";
-        return "0";
+    std::string resultbit = "1";
+    if(n1 == '0' && n2 == '0') {
+        *carry = '0';
+        return resultbit;
+    } else if(n1 == '0' && n2 == '1' || n1 == '1' && n2 == '0') {
+        *carry = '1';
+        resultbit = "0";
+        return resultbit;
     } else {
-        *carry = "1";
-        return "1";
+        *carry = '1';
+        return resultbit;
     }
 
 }
@@ -67,9 +69,9 @@ int main() {
         shortstring = n2;
     }
 
-    std::string carry = "0";
+    char carry = '0';
     for(int i = shorter; i > 0; i --) {
-        if(carry == "0") {
+        if(carry == '0') {
             resultstring = adder(
                 shortstring[i-1],
                 longstring[(longer - shorter) + i-1],
@@ -83,26 +85,34 @@ int main() {
             ) + resultstring;
         }
     }
+    
+    //std::cout << "First loop over, carry is: " << carry << std::endl;
+    //std::cout << "Resultstring is: " << resultstring << std::endl;
 
     int pos = longer-shorter-1;
-    while(carry = "1") {
+    while(carry == '1') {
         if(pos == -1) {
             resultstring = carry + resultstring;
             break;
         }
-        if(longstring[pos] == "0") {
-            resultstring = "1" + resultstring;
-            carry = "0";
+        if(longstring[pos] == '0') {
+            resultstring = '1' + resultstring;
+            carry = '0';
         } else {
-            resultstring = "0" + resultstring;
-            carry = "1";
+            resultstring = '0' + resultstring;
+            carry = '1';
         }
         pos --;
     }
-
+    
+    //std::cout << "Second loop over, carry is: " << carry << std::endl;
+    //std::cout << "Resultstring is: " << resultstring << std::endl;
+    
     if(pos > -1) {
-        resultstring = longer.substr(0, pos+1) + resultstring;
+        resultstring = longstring.substr(0, pos+1) + resultstring;
     }
+    
+    //std::cout << "Third loop over, carry is: " << carry << std::endl;
 
     std::cout << resultstring << std::endl;
 
